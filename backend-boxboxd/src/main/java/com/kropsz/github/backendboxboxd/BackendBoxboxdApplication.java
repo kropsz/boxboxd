89750b8a4258a0745bf8fce5ee494809706a411a
Pedro Esteves <kropinho211@outlook.com>
2024-05-15 11:12:13 -0300

    Initial commit

---
+++ backend-boxboxd/src/main/java/com/kropsz/github/backendboxboxd/BackendBoxboxdApplication.java
@@ -0,0 +1,13 @@
+package com.kropsz.github.backendboxboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBoxboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBoxboxdApplication.class, args);
+	}
+
+}
