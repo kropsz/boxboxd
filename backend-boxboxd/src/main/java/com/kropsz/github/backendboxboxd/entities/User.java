79c045f1420ff6b45d7b94c4e126ae0bf61137fd
Pedro Esteves <kropinho211@outlook.com>
2024-05-15 11:22:02 -0300

    Entity User and Role created

---
+++ backend-boxboxd/src/main/java/com/kropsz/github/backendboxboxd/entities/User.java
@@ -0,0 +1,43 @@
+package com.kropsz.github.backendboxboxd.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String username;
+    private byte photo;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+
+}
+
+
