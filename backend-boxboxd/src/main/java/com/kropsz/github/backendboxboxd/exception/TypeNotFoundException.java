e06a063259271804ab6fdca4302c95143fc23b13
Pedro Esteves <kropinho211@outlook.com>
2024-06-05 17:11:26 -0300

    Implemented new exception and updated exception handler

---
+++ backend-boxboxd/src/main/java/com/kropsz/github/backendboxboxd/exception/TypeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kropsz.github.backendboxboxd.exception;
+
+public class TypeNotFoundException extends RuntimeException{
+    public TypeNotFoundException(String message) {
+        super(message);
+    }
+}
